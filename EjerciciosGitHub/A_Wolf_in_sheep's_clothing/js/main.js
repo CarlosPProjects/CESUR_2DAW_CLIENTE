9a5fb36c576b432ac93dbcc178bd6f447b0ebbf4
Carlos Garavito <cg9822@gmail.com>
2022-01-01 18:57:01 +0100

    Funcionamiento básico y testing

--- EjerciciosGitHub/A_Wolf_in_sheep's_clothing/js/main.js
+++ EjerciciosGitHub/A_Wolf_in_sheep's_clothing/js/main.js
@@ -5,19 +5,20 @@
  * https://www.codewars.com/kata/5c8bfa44b9d1192e1ebd3d15
  */
 
-var farm = ['sheep', 'sheep', 'sheep', 'sheep', 'sheep', 'wolf'];
-var pivot;
-
-farm.sort();
-console.log()
-
-for (let i = farm.length; i > 0; i--) {
-    pivot = farm[i-1];
-
-    if(pivot == 'sheep') {
-        console.log("Oi! Sheep number "+ i + "! You are about to be eaten by a wolf!")
-    } else if(pivot == 'wolf') {
-        console.log("Pls go away and stop eating my sheep");
-    }
-}
-
+ var farm = ['sheep', 'sheep', 'sheep', 'sheep', 'sheep', 'wolf'];
+ var pivot;
+ 
+ farm.sort();
+ console.log()
+ 
+ for (let i = farm.length; i > 0; i--) {
+     pivot = farm[i-1];
+ 
+     if(pivot == 'sheep') {
+         console.log("Oi! Sheep number "+ i + "! You are about to be eaten by a wolf!")
+     } else if(pivot == 'wolf') {
+         console.log("Pls go away and stop eating my sheep");
+     }
+ }
+ 
+ 
